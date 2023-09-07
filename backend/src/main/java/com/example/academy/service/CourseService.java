c40222d8c042d640ceff6e06d03c1cd8af9fbdd8
LeoloFlrz <leoloflrzr@gmail.com>
2023-09-07 11:15:34 +0200

    created course service

---
+++ backend/src/main/java/com/example/academy/service/CourseService.java
@@ -0,0 +1,40 @@
+package com.example.academy.service;
+
+import com.example.academy.entity.Course;
+import com.example.academy.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public Course createCourse(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public Optional<Course> getCourseById(Long id) {
+        return courseRepository.findById(id);
+    }
+
+    public void updateCourse(Course course) {
+        courseRepository.save(course);
+    }
+
+    public void deleteCourse(Long id) {
+        Optional<Course> course = courseRepository.findById(id);
+        if (course.isEmpty()) {
+            System.out.println("Task doesn`t exist");
+        }
+        courseRepository.deleteById(id);
+    }
+}
---
+++ backend/src/main/java/com/example/academy/service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.academy.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
