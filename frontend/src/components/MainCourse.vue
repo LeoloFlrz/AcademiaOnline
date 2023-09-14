3a706d714bb942ed7eafa062b7cf0b455ab5fa3d
LeoloFlrz <leoloflrzr@gmail.com>
2023-09-14 15:26:57 +0200

    fixed some styles

--- frontend/src/components/MainCourse.vue
+++ frontend/src/components/MainCourse.vue
@@ -21,15 +21,13 @@ const courses = ref([])
 const courseId = ref()
 const courseTitle = ref()
 const courseDescription = ref()
+const courseRating = ref()
 
 const getUsers = async () => {
   let response = await ApiConnection.getAllUsers()
   users.value = response.data
-  console.log(users.value);
   userId.value = users.value[0].id
-  console.log(userId.value);
   userName.value = users.value[0].username
-  console.log(userName.value);
 }
 
 
@@ -39,31 +37,28 @@ const getUsers = async () => {
     courseTitle.value = courses.value[0].title
     courseId.value = courses.value[0].id
     courseDescription.value = courses.value[0].description
+    courseRating.value = courses.value[0].rating
   }
   
   onMounted(async () => {
-  // const response = await ApiConnection.getAllUsers()
-  // await getCourseById(1)
-  // courseTitle.value = response.data[0].title
-  // console.log(courseTitle.value);
   getCourses()
   getUsers()
 })
 </script>
 
 <template>
-  <div :class="courseClass">
+  <div class="flex justify-between" :class="courseClass">
     <div :class="videoClass">
       <video src="../assets/RaicesConSonido.mkv"></video>
     </div>
 
-    <div class="courseTitle">{{  }}</div>
+    <div class="courseTitle">{{ courseTitle }}</div>
 
-    <div class="courseDescription"></div>
+    <div class="courseDescription">{{ courseDescription }}</div>
 
     <div class="inscribeButton"></div>
 
-    <div class="courseRating"></div>
+    <div class="courseRating">Rating: {{ courseRating }}</div>
   </div>
 </template>
 
--- frontend/src/views/LandingView.vue
+++ frontend/src/views/LandingView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import MainButton from '../components/MainButton.vue';
 import MainCourse from '../components/MainCourse.vue';
 import MainNavbar from '../components/MainNavbar.vue';
 import VideoContainer from '../components/VideoContainer.vue';
@@ -11,14 +12,18 @@ import VideoContainer from '../components/VideoContainer.vue';
   </header>
 
   <main>
-    <div class="videoContainer">
-      <VideoContainer/>
+    <div class="videoContainer flex flex-row">
+      <VideoContainer class="w-10/12 h-auto"/>
       <p class="flex justify-center w-25">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis aliquid voluptate dicta quaerat ullam soluta adipisci inventore ducimus rerum, a assumenda perspiciatis nobis laborum, autem eaque amet reprehenderit illum eos?</p>
     </div>
 
-    <section class="flex justify-center">
-      <div class="coursesContainer bg-zinc-400 h-96 w-8/12 rounded-md">
-        <MainCourse video-class="w-96 h-auto"/>
+    <section class="flex flex-row justify-center items-center">
+      <div class="buttonContainer flex flex-end">
+        <MainButton button-title="Inscríbete"/>
+      </div>
+      
+      <div class="coursesContainer bg-zinc-400 h-auto w-11/12 mt-10 rounded-md">
+        <MainCourse video-class="w-96 h-auto" class=" m-10"/>
       </div>
     </section>
     
