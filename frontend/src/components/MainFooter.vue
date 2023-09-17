b5079d9bd3815a7b757929bb74fd384c96637bcf
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-09-17 17:02:11 +0200

    fixed footer size

--- frontend/src/components/MainFooter.vue
+++ frontend/src/components/MainFooter.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-<footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+<footer class="footer bg-white mt-20 shadow dark:bg-gray-900 ">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
@@ -30,4 +30,6 @@
   
 </template>
 
-<style scoped></style>
+<style scoped>
+    
+</style>
